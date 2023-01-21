589ad48c60ae7ad6bc227925578357dae099c505
HiccupHan <89772977+HiccupHan@users.noreply.github.com>
2023-01-21 03:30:00 -0800

    Added flower and player assets, spawn flowers

---
+++ Trashinator3000/Assets/changeCoord.cs
@@ -0,0 +1,19 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class changeCoord : MonoBehaviour
+// {
+//     public GameObject flowerSpawner;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+        
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         flowerSpawner.GetComponent<FlowerSpawner>()
+//     }
+// }
